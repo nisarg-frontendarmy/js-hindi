54a149dec621aae432063b24102cf3a4c62cfbd7
nisarg-frontendarmy <145429955+nisarg-frontendarmy@users.noreply.github.com>
2023-12-22 05:15:43 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js , not browser
+
+console.log( 3 
+    + 
+    3 )    // code readability should be high 
+
+console.log ("Nisarg")
+
+
+let name = "Nisarg"
+let age = 25
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined ); // undefined
+console.log(typeof null); // object
