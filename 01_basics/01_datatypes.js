545c911685cee2ed088a45b2515a192f0a88c3c0
nisarg-frontendarmy <145429955+nisarg-frontendarmy@users.noreply.github.com>
2023-12-22 05:43:32 +0000

    conversion of datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -28,3 +28,4 @@ let state;
 
 console.log(typeof undefined ); // undefined
 console.log(typeof null); // object
+
---
+++ 01_basics/02_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+//console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumebr = Number(score)
+// console.log(typeof valueInNumebr);
+// console.log(valueInNumebr);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1  false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Nisarg" => true
+
+let someNumebr = 25
+// console.log(typeof someNumebr);
+
+let stringNumber = String(someNumebr)
+console.log(stringNumber);
+console.log(typeof stringNumber);
