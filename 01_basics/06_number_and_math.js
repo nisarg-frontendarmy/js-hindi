63693e01400aba5ec51f16aa75481547bf147b75
nisarg-frontendarmy <145429955+nisarg-frontendarmy@users.noreply.github.com>
2023-12-22 09:12:01 +0000

    math and num

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance  = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+
+// console.log((otherNumber.toPrecision(3)));
+
+const hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+
+// ++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log((Math.abs(-5)));
+// console.log((Math.round(4.6)));
+// console.log((Math.ceil(4.1)));
+// console.log((Math.min(4 ,3,2,1)));
+// console.log((Math.max(4 ,3,2,1)));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log();(Math.random() * (max - min + 1))
